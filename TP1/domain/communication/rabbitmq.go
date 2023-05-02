cd6c56c117e8acfd348279c82e902663fada5ceb
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-05-01 21:43:06 -0300

    + Refactor Rabbit structs
    + Add fake broker service to docker compose

--- TP1/domain/communication/rabbitmq.go
+++ TP1/domain/communication/rabbitmq.go
@@ -1,24 +1,37 @@
 package communication
 
 type RabbitMQ struct {
-	Name              string            `yaml:"name"`
-	DeclarationConfig DeclarationConfig `yaml:"declaration_config"`
-	PublishingConfig  PublishingConfig  `yaml:"publishing_config"`
-	ConsumptionConfig ConsumptionConfig `yaml:"consumption_config"`
+	QueueDeclarationConfig    QueueDeclarationConfig    `yaml:"queue_declaration_config"`
+	ExchangeDeclarationConfig ExchangeDeclarationConfig `yaml:"exchange_declaration_config"`
+	PublishingConfig          PublishingConfig          `yaml:"publishing_config"`
+	ConsumptionConfig         ConsumptionConfig         `yaml:"consumption_config"`
 }
 
-// DeclarationConfig contains the parameters to declare a RabbitMQ queue
-type DeclarationConfig struct {
-	Durable          bool `yaml:"durable"`
-	DeleteWhenUnused bool `yaml:"delete_when_unused"`
-	Exclusive        bool `yaml:"exclusive"`
-	NoWait           bool `yaml:"no_wait"`
+// QueueDeclarationConfig contains the parameters to declare a RabbitMQ queue
+type QueueDeclarationConfig struct {
+	Name             string `yaml:"name"`
+	Durable          bool   `yaml:"durable"`
+	DeleteWhenUnused bool   `yaml:"delete_when_unused"`
+	Exclusive        bool   `yaml:"exclusive"`
+	NoWait           bool   `yaml:"no_wait"`
+}
+
+// ExchangeDeclarationConfig contains the parameters to declare a RabbitMQ exchange
+type ExchangeDeclarationConfig struct {
+	Name        string `yaml:"name"`
+	Type        string `yaml:"type"`
+	Durable     bool   `yaml:"durable"`
+	AutoDeleted bool   `yaml:"auto_deleted"`
+	Internal    bool   `yaml:"internal"`
+	NoWait      bool   `yaml:"no_wait"`
 }
 
 // PublishingConfig config use it for publishing messages in a RabbitMQ queue
 type PublishingConfig struct {
 	Exchange    string `yaml:"exchange"`
+	RoutingKey  string `yaml:"routing_key"`
 	Mandatory   bool   `yaml:"mandatory"`
+	Immediate   bool   `yaml:"immediate"`
 	ContentType string `yaml:"content_type"`
 }
 
