5bc84930b2c015d18d98a382b368910f710a9c36
Lisandro Torresetti <lisandro.ltorresetti@mercadolibre.com>
2023-05-09 01:54:48 -0300

    Rename directory

---
+++ TP1/eofmanager/eof_manager.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"regexp"
+	"time"
+	"tp1/communication"
+)
+
+const eofQueue = "eof-queue"
+
+type eofConfig struct {
+	counters  map[string]map[string]int `yaml:"counters"`
+	queues    []string                  `yaml:"queues"`
+	exchanges []string                  `yaml:"exchanges"`
+	responses map[string][]string       `yaml:"responses"`
+}
+
+type EOFManager struct {
+	config   *eofConfig
+	rabbitMQ *communication.RabbitMQ
+}
+
+type EOFMessageComponents struct {
+	Stage     string
+	City      string
+	ExtraData string
+}
+
+func getEOFComponents(eofMessage string) EOFMessageComponents {
+	regex := regexp.MustCompile(`^eof\.([^.]+)\.([^.]+)(.*)`)
+	matches := regex.FindStringSubmatch(eofMessage)
+
+	if len(matches) < 3 {
+		panic(fmt.Sprintf("[EOF Manager] invalid eof message %s", eofMessage))
+	}
+
+	extraData := ""
+	if len(matches) == 4 {
+		extraData = matches[3]
+	}
+
+	return EOFMessageComponents{
+		Stage:     matches[1],
+		City:      matches[2],
+		ExtraData: extraData,
+	}
+}
+
+func NewEOFManager(config *eofConfig, rabbitMQ *communication.RabbitMQ) *EOFManager {
+	return &EOFManager{
+		config:   config,
+		rabbitMQ: rabbitMQ,
+	}
+}
+
+func (eof *EOFManager) DeclareQueues() error {
+	/*var queuesDeclarations []communication.QueueDeclarationConfig
+	for _, queueName := range eof.config.queues {
+		queueDeclarationConfig := communication.QueueDeclarationConfig{
+			Name:             queueName,
+			Durable:          true,
+			DeleteWhenUnused: false,
+			Exclusive:        true,
+			NoWait:           false,
+		}
+		queuesDeclarations = append(queuesDeclarations, queueDeclarationConfig)
+	}*/
+	queueDeclarationConfig := communication.QueueDeclarationConfig{
+		Name:             eofQueue,
+		Durable:          true,
+		DeleteWhenUnused: false,
+		Exclusive:        true,
+		NoWait:           false,
+	}
+
+	err := eof.rabbitMQ.DeclareNonAnonymousQueues([]communication.QueueDeclarationConfig{queueDeclarationConfig})
+	if err != nil {
+		return err
+	}
+	log.Info("[EOF Manager] all queues were declared correctly")
+	return nil
+}
+
+func (eof *EOFManager) DeclareExchanges() error {
+	var exchangesDeclarations []communication.ExchangeDeclarationConfig
+	for _, exchangeName := range eof.config.exchanges {
+		exchangeDeclaration := communication.ExchangeDeclarationConfig{
+			Name:        exchangeName,
+			Type:        "topic",
+			Durable:     true,
+			AutoDeleted: false,
+			Internal:    false,
+			NoWait:      false,
+		}
+		exchangesDeclarations = append(exchangesDeclarations, exchangeDeclaration)
+	}
+
+	err := eof.rabbitMQ.DeclareExchanges(exchangesDeclarations)
+	if err != nil {
+		return err
+	}
+	log.Info("[eof manager] all exchanges were declared correctly")
+	return nil
+}
+
+func (eof *EOFManager) StartManaging() {
+	infinite := make(chan error, 1)
+
+	// weather filter handler
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		consumer, err := eof.rabbitMQ.GetQueueConsumer("eof-queue")
+		if err != nil {
+			infinite <- err
+		}
+
+		for message := range consumer {
+			eofMessage := string(message.Body)
+			eofComponents := getEOFComponents(eofMessage)
+			actualValue, ok := eof.config.counters[eofComponents.Stage][eofComponents.City]
+			if !ok {
+				panic(fmt.Sprintf("[EOF Manager] cannot found pair %s-%s", eofComponents.Stage, eofComponents.City))
+			}
+
+			updatedValue := actualValue - 1
+			if updatedValue < 0 {
+				panic(fmt.Sprintf("[EOF Manager] received more EOF than expected for pair %s-%s", eofComponents.Stage, eofComponents.City))
+			}
+
+			eof.config.counters[eofComponents.Stage][eofComponents.City] = updatedValue
+			if updatedValue == 0 {
+				err = eof.sendStartProcessingMessage(ctx, eofComponents)
+				if err != nil {
+					panic(err.Error())
+				}
+			}
+
+		}
+	}()
+
+	err := <-infinite
+	log.Errorf("[EOF Manager] got some error handling EOFs: %s", err.Error())
+}
+
+func (eof *EOFManager) sendStartProcessingMessage(ctx context.Context, eofComponents EOFMessageComponents) error {
+	targetExchanges, ok := eof.config.responses[eofComponents.Stage]
+	if !ok {
+		panic(fmt.Sprintf("[EOF Manager] cannot found exchange to send response for key %s", eofComponents.Stage))
+	}
+	routingKey := fmt.Sprintf("eof.%s.%s", eofComponents.Stage, eofComponents.City) // ToDo: potential bug. Licha
+	message := []byte(routingKey)
+	for _, exchange := range targetExchanges {
+		err := eof.rabbitMQ.PublishMessageInExchange(ctx, exchange, routingKey, message, "text/plain")
+		if err != nil {
+			log.Errorf("[EOF Manager] error publishing message in exchange %s", exchange)
+			return err
+		}
+	}
+	log.Infof("[EOF Manager] message sent correctly to exchanges: %v", targetExchanges)
+	return nil
+}
---
+++ TP1/eofmanager/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v3"
+	"os"
+	"tp1/communication"
+	"tp1/utils"
+)
+
+const configFilepath = "./manager/config/config.yaml"
+
+// InitLogger Receives the log level to be set in logrus as a string. This method
+// parses the string and set the level to the logger. If the level string is not
+// valid an error is returned
+func InitLogger(logLevel string) error {
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+
+	customFormatter := &log.TextFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		FullTimestamp:   false,
+	}
+	log.SetFormatter(customFormatter)
+	log.SetLevel(level)
+	return nil
+}
+
+func initEOFConfig() (*eofConfig, error) {
+	configFile, err := utils.GetConfigFile(configFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var cfg eofConfig
+	err = yaml.Unmarshal(configFile, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing EOF Manager config file: %s", err)
+	}
+
+	return &cfg, nil
+}
+
+func main() {
+	logLevel := os.Getenv("LOG_LEVEL")
+	if logLevel == "" {
+		logLevel = "DEBUG"
+	}
+
+	if err := InitLogger(logLevel); err != nil {
+		log.Fatalf("%s", err)
+		return
+	}
+
+	eofManagerConfig, err := initEOFConfig()
+	if err != nil {
+		log.Fatalf("%s", err)
+		return
+	}
+
+	rabbitMQ, err := communication.NewRabbitMQ()
+	if err != nil {
+		log.Errorf("[EOF Manager] error getting RabbitMQ instance: %s", err.Error())
+		return
+	}
+
+	defer func(rabbitMQ *communication.RabbitMQ) {
+		err := rabbitMQ.KillBadBunny()
+		if err != nil {
+			log.Errorf("[EOF Manager] error killing RabbitMQ instance: %s", err.Error())
+		}
+	}(rabbitMQ)
+
+	eofManager := NewEOFManager(eofManagerConfig, rabbitMQ)
+
+	err = eofManager.DeclareQueues()
+	if err != nil {
+		log.Errorf("[EOF Manager] error declaring queues: %s", err.Error())
+		return
+	}
+
+	err = eofManager.DeclareExchanges()
+	if err != nil {
+		log.Errorf("[EOF Manager] error declaring exchanges: %s", err.Error())
+		return
+	}
+
+	eofManager.StartManaging()
+	log.Debug("[EOF Manager] finish main.go")
+}
